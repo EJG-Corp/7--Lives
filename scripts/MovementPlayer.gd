eba069992b01e7fef9af8b0a458fe81328ae3eb7
gonzalo <gonzalo.javier.cartes@gmail.com>
2024-04-08 12:54:55 -0400

    feat(player movement): El personaje principal se mueve
    Los controles son W,A,S,D . El personaje se mueve de izquierda a derecha, y si está en el piso puede saltar con W.

--- scripts/MovementPlayer.gd
+++ scripts/MovementPlayer.gd
@@ -1,13 +1,20 @@
 extends CharacterBody2D
 
-@export var speed = 400
-var gravity = 100
-var acceleration = 300
+var speed = 400
+var jump_speed = 500
+var gravity = 900
+var acceleration = 2000
 
-func _procces(delta:float) -> void:
+
+func _physics_process(delta:float) -> void:
 	if not is_on_floor():
 		velocity.y += gravity * delta
+	
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		velocity.y = -jump_speed
 		
+				
 	var move_input = Input.get_axis("move_left","move_right")
 	velocity.x = move_toward(velocity.x,move_input*speed,acceleration*delta)
 	
+	move_and_slide()
