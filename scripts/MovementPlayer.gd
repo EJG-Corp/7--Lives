3885b62c844fb69f40e9ec021c288565281bad97
Jorge Cruces <jcrucesdeveloper@gmail.com>
2024-05-13 11:53:02 -0400

    feat(animation): animacion agregadas al proyecto

--- scripts/MovementPlayer.gd
+++ scripts/MovementPlayer.gd
@@ -5,6 +5,8 @@ var jump_speed = 300
 var gravity = 900
 var acceleration = 2000
 
+@onready var animation_tree: AnimationTree = %AnimationTree
+@onready var playback = animation_tree.get("parameters/playback")
 
 func _physics_process(delta:float) -> void:
 	if not is_on_floor():
@@ -12,9 +14,17 @@ func _physics_process(delta:float) -> void:
 	
 	if is_on_floor() and Input.is_action_just_pressed("jump"):
 		velocity.y = -jump_speed
-		
-				
+					
 	var move_input = Input.get_axis("move_left","move_right")
 	velocity.x = move_toward(velocity.x,move_input*speed,acceleration*delta)
 	
+	handle_animation()
 	move_and_slide()
+
+
+func handle_animation():
+	if is_on_floor():
+		if abs(velocity.x) > 10:
+			playback.travel("Run")
+		else:
+			playback.travel("Idle")
