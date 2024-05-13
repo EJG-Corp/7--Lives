260c1d08a72d51b78df73a11e82076b5b4b4c66c
gonzalo <gonzalo.javier.cartes@gmail.com>
2024-05-13 13:37:59 -0400

    feat(new scenes and fixed animations)
    Nuevas escenas añadidas para teletransportar al jugador al siguiente nivel . Un pequeño fix fue que se centraron los nodos y sprites del jugador para que las posiciones tengan más sentido . La animación de correr funciona para ambos sentidos

--- scripts/MovementPlayer.gd
+++ scripts/MovementPlayer.gd
@@ -5,6 +5,7 @@ var jump_speed = 300
 var gravity = 900
 var acceleration = 2000
 
+@onready var pivot : Node2D = %Pivot
 @onready var animation_tree: AnimationTree = %AnimationTree
 @onready var playback = animation_tree.get("parameters/playback")
 
@@ -18,13 +19,24 @@ func _physics_process(delta:float) -> void:
 	var move_input = Input.get_axis("move_left","move_right")
 	velocity.x = move_toward(velocity.x,move_input*speed,acceleration*delta)
 	
-	handle_animation()
+	if is_on_floor():
+		
+		if abs(velocity.x) > 10 or move_input != 0:
+			playback.travel("Run")
+		else:
+			playback.travel("Idle")
+		
+		if move_input :
+			pivot.scale.x = sign(move_input)
+	
+
+	#handle_animation()
 	move_and_slide()
 
 
 func handle_animation():
 	if is_on_floor():
-		if abs(velocity.x) > 10:
+		if abs(velocity.x) > 10 :
 			playback.travel("Run")
 		else:
 			playback.travel("Idle")
